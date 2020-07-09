f1499417becc3cc1a4d996f73a7a4030ce6828ec
FishPlusOrange <hi@fishplusorange.com>
2020-07-09 23:31:22 +0800

    chore: optimize release scripts

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,16 +1,24 @@
-#!/bin/bash
+#!/usr/bin/env sh
 
 set -e
 echo "Enter release version:"
 read VERSION
+
 read -p "Release version $VERSION - are you sure? (y/n)" -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-# update version
-npm version $VERSION --message "Release $VERSION"
-git push origin master tags/v$VERSION
+  echo "Releasing $VERSION ..."
 
-# publish package
-npm publish --access public --registry=https://registry.npmjs.org
+  npm version $VERSION --message "Release $VERSION"
+
+  BRANCH=$(git symbolic-ref --short HEAD)
+  git push origin $BRANCH
+  if [ $BRANCH == 'master' ]
+  then
+    echo "Add tag v$VERSION"
+    git push origin refs/tags/v$VERSION
+  fi
+
+  npm publish --access public --registry=https://registry.npmjs.org
 fi
